53ff375f318874555fd42b8bf4028349b1ea66d9
Tom_XV <GitHub@lugiaxetomxv.net>
2026-09-12 05:43:00 +0900

    Start the installer without leaving a console window behind

--- installer/Install.cmd
+++ installer/Install.cmd
@@ -1,3 +1,2 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -STA -File "%~dp0installer\Installer.ps1"
-if errorlevel 1 pause
+start "" powershell -NoProfile -ExecutionPolicy Bypass -STA -WindowStyle Hidden -File "%~dp0installer\Installer.ps1"
